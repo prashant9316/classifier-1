a2665512ba9e79a470e077d54f6d276fe0cc30ff
Prashant Mishra <48996523+prashant9316@users.noreply.github.com>
2020-06-14 19:19:39 +0530

    Add files via upload

---
+++ main/js/facerecog.js
@@ -0,0 +1,92 @@
+let model;
+const compare = tf.tensor1d([0.5], dtype = 'float32')
+const webcam = new Webcam(document.getElementById('wc'));
+let isPredicting = false;
+
+async function loadModel(){
+    model = await tf.loadLayersModel("model/model.json");
+    console.log('Var: '+compare)
+    return (model);
+    
+}
+
+async function predict(){
+    while(isPredicting){
+        const output = tf.tidy(() => {
+            const img = webcam.capture();
+            const prediction =  model.predict(img);
+            return prediction;
+        });
+        classId = await output;
+        console.log(classId);
+        console.log("output: "+ output);
+        if(classId >= 0.5){
+            predictionText = "I see a Dog";
+        }
+        else if(classId<0.5){
+            predictionText = "I see a Cat";
+        }
+        else{
+            console.log("output: "+ output);
+            predictionText = output;
+        }
+        document.getElementById("prediction").innerText = predictionText;
+        
+        output.dispose();
+        await tf.nextFrame();
+    }
+}
+
+async function getPrediction(img){
+    const output = tf.tidy(() => {
+        const prediction = model.predict(img);
+        return prediction.as1D().argMax();
+    })
+    const classId = (await output.data())[0];
+    var predText = " ";
+    console.log(classId);
+        if(classId >= 0.5){
+            predictionText = "I see a Dog";
+        }
+        else if(classId<0.5){
+            predictionText = "I see a Cat";
+        }
+        else{
+            console.log("output: "+ output)
+            predictionText = "Invalid";
+        }
+    document.getElementById("pred").innerText = predText;
+    output.dispose();
+}
+
+function showModel(){
+    tfvis.show.modelSummary({name:'Model Architecture'}, model);
+}
+
+function startPredicting(){
+    isPredicting = true;
+    predict();
+}
+
+function stopPredicting(){
+    isPredicting = false;
+    predict();
+}
+
+function saveModel(){
+    model.save("downloads://cat_dog_classifier");
+}
+
+
+    
+async function init(){
+    console.log("Loading the classifier model");
+    model = loadModel();
+    console.log('The model is loaded!');
+    console.log("setting up the webcam");
+    await webcam.setup();
+    console.log("webcam setup done!")
+    
+}
+
+init();
